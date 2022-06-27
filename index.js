c8c4722d93f83356d994506e2fde51b4c7fbd388
Joshua <joshsmayhew@gmail.com>
2022-06-27 17:15:48 +0700

    commit tree 3306953e2354e69821347e20551a409f910cfe1c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+  console.log("This console.log() fired when index.js loades - before DomContentLoaded is triggered")
+
+  function changeTextAfterLoading() {
+    const text = document.querySelector('p');
+    text.textContent = "This is really cool!";
+    console.log("This is executed after the DOM has loaded");
+  }
+
+  document.addEventListener("DOMContentLoaded", changeTextAfterLoading);
